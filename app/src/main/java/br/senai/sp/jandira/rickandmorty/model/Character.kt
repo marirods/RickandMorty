627973c90cf93b40766a606599711366c1023038
MARIANE DA SILVA RODRIGUES <24122311@dsn127m1249389.cfp127aluno.local>
2025-05-13 11:21:18 -0300

    ok

---
+++ app/src/main/java/br/senai/sp/jandira/rickandmorty/model/Character.kt
@@ -0,0 +1,12 @@
+package br.senai.sp.jandira.rickandmorty.model
+
+data class Character(
+    var id: Int = 0,
+    var name: String = "",
+    var status: String = "",
+    var species: String = "",
+    var image: String = ""
+)
+
+
+
---
+++ app/src/main/java/br/senai/sp/jandira/rickandmorty/model/Result.kt
@@ -0,0 +1,5 @@
+package br.senai.sp.jandira.rickandmorty.model
+
+data class Result(
+    var results: List<Character>?= null
+)
